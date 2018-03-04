a3ff3e02643d389ed808b92aa187700840468d44
Malte Ubl <malte.ubl@gmail.com>
2018-03-04 11:26:44 -0800

    Rehash because now new HTML files can change what CSS we need to prune.

--- scripts/purify-css.js
+++ scripts/purify-css.js
@@ -1,5 +1,8 @@
 const purifycss = require('purify-css');
 const find = require('find');
+const path = require('path');
+const fs = require('fs');
+const replace = require('replace-in-file');
 
 const content = ['build/**/*.js', 'build/**/*.html'];
 
@@ -12,8 +15,30 @@ find.file(/main\.css$/, 'build/', files => {
   purifycss(content, files, {
     // Comment in for debugging.
     // rejected: true,
-
+  }, css => {
     // Change in-place
-    output: files[0],
+    console.log('Done purifying');
+    rehash(files[0], css);
   });
 });
+
+// Rehash because now new HTML files can change what CSS we need to prune
+function rehash(filename, css) {
+  const hash = require('crypto').createHash('sha1');
+  hash.update(css, 'utf8');
+  const digest = hash.digest('hex');
+  fs.writeFileSync(filename, css);
+  const parts = filename.split(path.sep);
+  const oldDigest = parts[2];
+  const prefix = parts[0] + path.sep + parts[1];
+  const oldDir = prefix + path.sep + oldDigest;
+  const newDir = prefix + path.sep + digest;
+  console.log('Renaming', oldDir, 'to', newDir);
+  fs.renameSync(oldDir, newDir);
+  // Replace the references to the digest in the HTML files.
+  console.log('Replacing digest', replace.sync({
+    files: 'build/**/*.html',
+    from: oldDigest,
+    to: digest,
+  }));
+}
