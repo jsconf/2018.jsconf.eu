5c0dbd4b1cb964d95b0fe519b3886118ea73d58d
Malte Ubl <malte.ubl@gmail.com>
2018-03-04 11:59:22 -0800

    ⚡  Optimize the purchase flow by prefetching and preconnecting from/to ti.to
    Prefetches the main doc and known sub resources whenever a doc finishes loading.
    
    On mouseover and click of the purchase button, preconnects to ti.to and other origins that ti.to uses to flatten the connection tree.

--- contents/js/main.js
+++ contents/js/main.js
@@ -9,10 +9,52 @@
     topbarNav.classList.toggle('topbar-nav--is-visible');
   });
 
+  Array.from(document.querySelectorAll('.buy-ticket')).forEach(function(buy) {
+    buy.ontouchstart = buy.onmouseover = preconnectTito;
+  });
 })();
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js');
-  });
+  }
+  track();
+  prefetchTito();
+});
+
+function preconnectTito() {
+  addLink('preconnect', 'https://ti.to');
+  addLink('preconnect', 'https://use.typekit.com');
+  addLink('preconnect', 'https://d2z6c3c3r6k4bx.cloudfront.net');
+  addLink('preconnect', 'https://titoproduction.global.ssl.fastly.net');
+  addLink('preconnect', 'https://maps.googleapis.com');
+}
+
+function prefetchTito() {
+  // Only do this before the event
+  if (Date.now() > new Date('2018-07-01').getTime()) {
+    return;
+  }
+  addLink('prefetch', 'https://ti.to/jsconfeu/jsconf-eu-2018');
+  addLink('prefetch', 'https://js.stripe.com/v2/');
+  addLink('prefetch', 'https://use.typekit.com/zjo8qhc.js');
+}
+
+function addLink(rel, href) {
+  const link = document.createElement('link');
+  link.rel = rel;
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+var _gauges = _gauges || [];
+function track() {
+  var t   = document.createElement('script');
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '50570251f5a1f57282000003');
+  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+  t.src = 'https://d36ee2fcip1434.cloudfront.net/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
 }
