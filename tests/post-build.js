eebfcd5fac2da45b626ef96b6439743c712e8eeb
Malte Ubl <malte.ubl@gmail.com>
2018-04-10 14:37:23 -0700

    For virtual pages test the input documents instead since the output is not generated.

--- tests/post-build.js
+++ tests/post-build.js
@@ -14,9 +14,10 @@ function checkDir(name, minCount) {
   }
 }
 
+checkDir('contents/speakers/', 40);
 checkDir('build/speakers/', 40);
-checkDir('build/sponsors/', 5);
-checkDir('build/mcs/', 3);
-checkDir('build/team/', 3);
+checkDir('contents/sponsors/', 5);
+checkDir('contents/mcs/', 3);
+checkDir('contents/team/', 3);
 checkDir('contents/images/speaker/', 40);
 checkDir('contents/images/sponsor/', 5);
