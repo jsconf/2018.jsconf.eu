3f50998306c38bf849ce573d4c4781766c0e62fb
Malte Ubl <cramforce@users.noreply.github.com>
2018-04-10 21:30:31 -0700

    Ping

--- contents/js/main.js
+++ contents/js/main.js
@@ -45,6 +45,7 @@ function prefetchTito() {
   addLink('prefetch', 'https://use.typekit.com/zjo8qhc.js');
   addLink('prefetch', 'https://d2z6c3c3r6k4bx.cloudfront.net/uploads/event/banner/1054103/a33a41ce71c1fe41b39a88c1e37ad6e1.png');
   addLink('prefetch', 'https://d2z6c3c3r6k4bx.cloudfront.net/uploads/event/logo/1054103/46794018cff5e643871986714e1f1676.png');
+  
   preconnectTito();
 }
 
