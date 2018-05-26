cd576e5f98d264e67546ba465e1724c304db5488
Malte Ubl <malte.ubl@gmail.com>
2018-05-26 14:23:43 +0200

    Sold out

--- contents/js/main.js
+++ contents/js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-Array.from(document.querySelectorAll('.buy-ticket')).forEach(function(buy) {
-  buy.addEventListener('touchstart', preconnectTito, {
-    passive: true,
-    capture: true,
-  });
-  buy.onmouseover = preconnectTito;
-});
-
 var ranOnce = false;
 function executeEarlyish() {
   if (ranOnce) {
@@ -16,7 +8,6 @@ function executeEarlyish() {
   ranOnce = true;
   addLink('prefetch', '/news/schedule/');
   addLink('prefetch', '/schedule/');
-  prefetchTito();
   pingTypekit();
 }
 
@@ -26,29 +17,7 @@ window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js');
   }
 });
-// setTimeout(executeEarlyish, 500);
-
-function preconnectTito() {
-  addLink('preconnect', 'https://ti.to');
-  addLink('preconnect', 'https://use.typekit.com');
-  addLink('preconnect', 'https://d2z6c3c3r6k4bx.cloudfront.net');
-  addLink('preconnect', 'https://titoproduction.global.ssl.fastly.net');
-  addLink('preconnect', 'https://maps.googleapis.com');
-}
-
-function prefetchTito() {
-  // Only do this before the event
-  if (Date.now() > new Date('2018-07-01').getTime()) {
-    return;
-  }
-  addLink('prefetch', 'https://ti.to/jsconfeu/jsconf-eu-2018');
-  addLink('prefetch', 'https://js.stripe.com/v2/');
-  addLink('prefetch', 'https://use.typekit.com/zjo8qhc.js');
-  addLink('prefetch', 'https://d2z6c3c3r6k4bx.cloudfront.net/uploads/event/banner/1054103/a33a41ce71c1fe41b39a88c1e37ad6e1.png');
-  addLink('prefetch', 'https://d2z6c3c3r6k4bx.cloudfront.net/uploads/event/logo/1054103/46794018cff5e643871986714e1f1676.png');
-
-  preconnectTito();
-}
+setTimeout(executeEarlyish, 500);
 
 function addLink(rel, href) {
   const link = document.createElement('link');
