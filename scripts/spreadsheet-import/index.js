06627704202f37d9c7e59307b94b9e15ee0e09b9
Malte Ubl <malte.ubl@gmail.com>
2018-05-29 18:04:50 +0200

    Fix schecule import and unify time format.
    …in schedule to use dot as the hour minute separator.

--- scripts/spreadsheet-import/index.js
+++ scripts/spreadsheet-import/index.js
@@ -168,6 +168,14 @@ async function main(params) {
   // ---- parse and generate markdown-files
   console.log(chalk.gray('awesome, that worked.'));
   Object.keys(sheets).forEach(sheetId => {
+    if (!sheetId) {
+      // Published pages create unnamed sheets.
+      return;
+    }
+    if (!sheetParams[sheetId]) {
+      console.log(chalk.red('Missing metadata for'), sheetId);
+      return;
+    }
     const {templateGlobals, dataFieldName, contentPath, parseSchedule} = sheetParams[sheetId];
     if (parseSchedule) {
       processSchedule(sheets[sheetId]);
